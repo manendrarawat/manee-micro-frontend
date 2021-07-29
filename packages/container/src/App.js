d850753283a28f9c2e3aa188e8a8a058ea0e00de
manendra.rawat <mannuatnri.itm@gmail.com>
2021-07-29 18:55:40 +0530

    updated container to run build

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,7 +3,7 @@ import MarketingApp from './components/MarketingApp';
 
 export default () => {
 
-    console.log('testing comment six :>> ');
+    console.log('testing comment seven :>> ');
     return (<div>
                 <h1>Hi There !</h1>
                 <hr />
