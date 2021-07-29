2f4b2f1c3e1b0261d9795d9f12d07a69188a6ed0
manendra.rawat <mannuatnri.itm@gmail.com>
2021-07-29 17:35:25 +0530

    testing comment four

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,7 +3,7 @@ import MarketingApp from './components/MarketingApp';
 
 export default () => {
 
-    console.log('testing comment three :>> ');
+    console.log('testing comment four :>> ');
     return (<div>
                 <h1>Hi There !</h1>
                 <hr />
